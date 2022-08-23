a9dd9921f67f563f71951464c8953f9c8f00f108
newbiejas <jaskarannagra@homail.com>
2022-08-22 18:14:45 -0700

    start on menu component using bootstrap cards to display menu items

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,10 +4,17 @@ import styles from '../Styles/Home.module.css'
 const Home = () => {
     return (
         <div className={styles.homeContainer}>
-            <div >
+            <div className={styles.quoteContainer}>
                 <p className={styles.quote}>Some of the best Smoked Brisket ive ever had</p>
+                <h4> - Evan Penner </h4>
             </div>
-            <div>
+            <div className={styles.hoursContainer}>
+                <h5>Hours</h5>
+                <p>Monday - Friday</p>
+                <p>0900 - 0500</p>
+                <p>Saturday - Sunday</p>
+                <p>Closed</p>
+ 
 
             </div>
         </div>
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import image from '../Images/brisket.jpg'
+import styles from'../Styles/Menu.module.css'
 
 const Menu = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-export default Menu
+  return (
+    <div className={styles.menuContainer}>
+      <div className='d-flex p-5'>
+        <Card bg='dark' text='light' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title>10 lb Smoked Brisket</Card.Title>
+            <Card.Text>
+              10lb Brisket smoked for 12 hours with Applewood pellets
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+};
+export default Menu;
