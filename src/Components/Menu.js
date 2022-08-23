4939ee7e641c8b1f9773d40907b36bb3257e1f7c
newbiejas <jaskarannagra@homail.com>
2022-08-22 19:30:47 -0700

    style

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -19,32 +19,69 @@ const Menu = () => {
     <div className={styles.menuContainer}>
       <div className="container">
         <div className="row p-5">
-          <Card bg="dark" text="light" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={brisket} />
-            <Card.Body>
-              <Card.Title>10 lb Smoked Brisket</Card.Title>
-              <Card.Text>
-                10lb Brisket smoked for 12 hours with Applewood pellets
-              </Card.Text>
-              <Button variant="primary" onClick={() => handleClick(menuItems[0])}>
-                Add to Cart
-              </Button>
-            </Card.Body>
-          </Card>
+          <div className="d-flex justify-content-evenly">
+            <div>
+              <Card bg="dark" text="light" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={brisket} />
+                <Card.Body>
+                  <Card.Title>10 lb Smoked Brisket</Card.Title>
+                  <Card.Text>
+                    10lb Brisket smoked for 12 hours with Applewood pellets
+                  </Card.Text>
+                  <Button variant="primary" onClick={() => handleClick(menuItems[0])}>
+                    Add to Cart
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+            <div>
+              <Card bg="dark" text="light" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={brisket} />
+                <Card.Body>
+                  <Card.Title>10 lb Smoked Brisket</Card.Title>
+                  <Card.Text>
+                    10lb Brisket smoked for 12 hours with Applewood pellets
+                  </Card.Text>
+                  <Button variant="primary" onClick={() => handleClick(menuItems[0])}>
+                    Add to Cart
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
         </div>
         <div className="row p-5">
-          <Card bg="dark" text="light" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={pork} />
-            <Card.Body>
-              <Card.Title>10 lb Pulled Pork</Card.Title>
-              <Card.Text>
-                10lb Boston Butt smoked for 12 hours with Applewood pellets
-              </Card.Text>
-              <Button variant="primary" onClick={() => handleClick(menuItems[1])}>
-                Add to Cart
-              </Button>
-            </Card.Body>
-          </Card>
+          <div className="d-flex justify-content-evenly">
+            <div>
+              <Card bg="dark" text="light" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={pork} />
+                <Card.Body>
+                  <Card.Title>10 lb Pulled Pork</Card.Title>
+                  <Card.Text>
+                    10lb Boston Butt smoked for 12 hours with Applewood pellets
+                  </Card.Text>
+                  <Button variant="primary" onClick={() => handleClick(menuItems[1])}>
+                    Add to Cart
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+            <div>
+              <Card bg="dark" text="light" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={pork} />
+                <Card.Body>
+                  <Card.Title>10 lb Pulled Pork</Card.Title>
+                  <Card.Text>
+                    10lb Boston Butt smoked for 12 hours with Applewood pellets
+                  </Card.Text>
+                  <Button variant="primary" onClick={() => handleClick(menuItems[1])}>
+                    Add to Cart
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+            
+          </div>
         </div>
       </div>
     </div>
