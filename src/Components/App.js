0e249ca2ab74ce0e962e45873228b599ed9d91e9
newbiejas <jaskarannagra@homail.com>
2022-08-22 18:36:33 -0700

    create shopping cart context to share state between components that are not direct descendants of each other

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import DarkNavbar from './DarkNavbar'
 import Menu from './Menu';
 import Home from './Home'
+import Cart from './Cart'
 import '../Styles/App.css';
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/menu' element={<Menu />} />
+        <Route path='/cart' element={<Cart />} />
+
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import image from '../Images/brisket.jpg'
-import styles from'../Styles/Menu.module.css'
+import brisket from '../Images/brisket.jpg'
+import pork from '../Images/Pork-Butt.jpg'
+import styles from '../Styles/Menu.module.css'
 
 const Menu = () => {
   return (
     <div className={styles.menuContainer}>
-      <div className='d-flex p-5'>
+      <div className='row p-5'>
         <Card bg='dark' text='light' style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={image} />
+          <Card.Img variant="top" src={brisket} />
           <Card.Body>
             <Card.Title>10 lb Smoked Brisket</Card.Title>
             <Card.Text>
@@ -19,6 +20,19 @@ const Menu = () => {
           </Card.Body>
         </Card>
       </div>
+      <div className='row p-5'>
+        <Card bg='dark' text='light' style={{ width: '18rem'}}>
+          <Card.Img variant="top" src={pork} />
+          <Card.Body>
+            <Card.Title>10 lb Pulled Pork</Card.Title>
+            <Card.Text>
+              10lb Boston Butt smoked for 12 hours with Applewood pellets
+            </Card.Text>
+            <Button variant="primary" onClick={()=>console.log('test')}>Add to Cart</Button>
+          </Card.Body>
+        </Card>
+      </div>
+
     </div>
   );
 };
