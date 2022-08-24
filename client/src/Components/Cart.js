4b48aa7f3794bcb9825ae1b0ddfe33251d724bb1
newbiejas <jaskarannagra@homail.com>
2022-08-23 17:05:48 -0700

    test

---
+++ client/src/Components/Cart.js
@@ -0,0 +1,12 @@
+import React, {useContext} from "react";
+import { CartContext } from "./CartContext";
+
+const Cart = () => {
+    const [cart,setCart] = useContext(CartContext)
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default Cart
