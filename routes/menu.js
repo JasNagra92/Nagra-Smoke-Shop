af3a79cd5ab9ccb3c549eb216d5d57462b9cff72
newbiejas <jaskarannagra@homail.com>
2022-08-24 13:09:24 -0700

    test

--- routes/menu.js
+++ routes/menu.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 
 router.get('/menu', (req, res) => {
     const db = mongoose.connection.db;
-    db.collection('menuItems').find().toArray((err, result) => {
+    db.collection('menuItems').find({}).project({_id:1}).toArray((err, result) => {
         if (err) {
             res.status(400).json({err:err})
         }
