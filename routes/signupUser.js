47e664b8911edc97f8b52973101b821ccffb0d05
jasmac <jaskarannagra@hotmail.com>
2022-09-24 12:41:54 -0700

    attach database user _id to jwt token that is sent to client after user signup

--- routes/signupUser.js
+++ routes/signupUser.js
@@ -4,7 +4,7 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
-  return jwt.sign({}, process.env.JWT_SECRET, { expiresIn: "3d" });
+  return jwt.sign({_id}, process.env.JWT_SECRET, { expiresIn: "3d" });
 };
 
 router.post("/", async (req, res) => {
